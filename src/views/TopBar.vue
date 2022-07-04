76a80974df7150a855f7629f0dcc187483c965c7
gn01792218 <gn01792218@hotmail.com>
2022-07-04 16:14:12 +0800

    新增語系下拉選單

--- src/views/TopBar.vue
+++ src/views/TopBar.vue
@@ -1,8 +1,24 @@
 <template>
-  <h1 class="bg-black text-white">使用帳號:{{userName}}</h1>
+<div class="flex justify-between">
+  <div>
+    <h1 class="bg-black text-white">使用帳號:{{userName}}</h1>
+  </div>
+  <div class="w-[433px] h-[255px]">
+    <img class="w-full" src="../assets/images/YGR_logo.png" alt="YGR官網LOGO">
+  </div>
+  <div>
+    <select name="" id="">
+      <option
+        v-for ="lang in Lang" :key="lang"
+      :value="lang">{{lang}}</option>
+      </select>
+  </div>
+</div>
+  
 
 </template>
 <script setup lang="ts">
-import useUser from "../composable/user/useUser"
+import {Lang} from "@/types/lang"
+import useUser from "@/composable/user/useUser"
 const {userName} = useUser()
 </script>
