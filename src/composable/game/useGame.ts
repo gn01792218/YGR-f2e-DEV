3066fea4f5c69f8db0999d2053404dd46a36c5a2
gn01792218 <gn01792218@hotmail.com>
2022-07-05 13:54:03 +0800

    遊戲圖片依據語系自動轉換對應圖檔

--- src/composable/game/useGame.ts
+++ src/composable/game/useGame.ts
@@ -3,9 +3,10 @@ import { useStore } from "vuex";
 import {Lang} from '@/types/lang';
 import { GameCategory } from "@/types/game/game";
 import useGameList from "./useGameList";
-
+import useUtil from '@/composable/util/useUtil'
 export default function useGame(){
     const store = useStore()
+    const { getAssetsFileURL } = useUtil()
     const {gameList} = useGameList()
     const gameLang = computed<Lang>(()=>{
         return store.state.game.lang
@@ -13,7 +14,16 @@ export default function useGame(){
     const gameCategory = computed(()=>{
         return store.state.game.gameCategory
     })
-
+    const gameImg = computed(()=>{
+        
+    })
+    function getGameImgUrl(gameid:string){
+        if(gameLang.value ===Lang["zh-CN"]){
+            return getAssetsFileURL(`images/gameIcon/${gameid}_cn.png`)
+        }else{
+           return  getAssetsFileURL(`images/gameIcon/${gameid}_en.png`)
+        }
+    }
     function switchGameCategory(category:GameCategory){
         store.commit("game/sethGameCategory",category)
     }
@@ -22,8 +32,10 @@ export default function useGame(){
         gameList,
         gameLang,
         gameCategory,
+        gameImg,
         //methods
         switchGameCategory,
+        getGameImgUrl,
        
     }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,12 @@
   <GameSwitchTab :tabList="[GameCategory.ALLGAME, GameCategory.SLOT, GameCategory.FISH]" />
   <div class="flex flex-wrap ml-3">
     <div v-for="card in gameList" :key="card.GameId">
-      <GameCard class="mr-5"
+      <GameCard class="m-2"
       v-show="gameCategory===card.GameCategoryId || gameCategory===0" 
       :card="{
         gameId: card.GameId,
         lang: Lang['zh-CN'],
-        imgSrc: getAssetsFileURL(`images/gameIcon/${card.GameId}_cn.png`),
+        imgSrc:getGameImgUrl(card.GameId),
         gameCategoryId: card.GameCategoryId
       }" />
     </div>
@@ -18,12 +18,12 @@
 <script setup lang="ts">
 import { GameCategory } from "@/types/game/game"
 import { Lang } from "@/types/lang"
-import useUtil from '@/composable/util/useUtil'
+
 import useGame from '@/composable/game/useGame'
 import GameCard from '@/components/GameCard.vue'
 import TopBar from '@/components/TopBar.vue'
 import GameSwitchTab from '@/components/GameSwitchTab.vue'
 
-const { getAssetsFileURL } = useUtil()
-const { gameList,gameCategory} = useGame()
+
+const { gameList,gameCategory,getGameImgUrl} = useGame()
 </script>
