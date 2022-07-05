92243f70b8902479a14ef5b1af1723a075e1dde7
gn01792218 <gn01792218@hotmail.com>
2022-07-05 14:21:54 +0800

    修改設計系統的顏色class

--- src/components/GameSwitchTab.vue
+++ src/components/GameSwitchTab.vue
@@ -1,5 +1,5 @@
 <template>
-    <ul class="w-full flex justify-center grey-400 my-10">
+    <ul class="w-full flex justify-center grey-bg-400 my-10">
         <li class="mr-5 cursor-pointer"
             v-for="tab in tabList" :key="tab"
             @click="switchGameCategory(tab)"
