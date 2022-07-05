074cc358c99949c212efb8f8799bf10c291594b9
gn01792218 <gn01792218@hotmail.com>
2022-07-05 15:08:52 +0800

    新增gh-page部署插件、刪除不必要的API

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,6 @@
 //引入你配置的axios
 import req from './http'
 
-//API-GET
-export const getIntoGame = (account:string,gameID:string,lang:string)=>{
-    return req('get',`Login?Account=${account}&GameId=${gameID}&Lang=${lang}`)
-}
-
 //API-POST
 export const getGameList = () => {
   return req('post',`/GameList`,{})
